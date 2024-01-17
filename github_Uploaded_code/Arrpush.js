4967d2cc7575db8f5ed5cd9f1a00b4865ce56a7a
Mahendra Singh <126326125+Mahendra0115@users.noreply.github.com>
2024-01-17 16:37:12 +0530

    Add files via upload

---
+++ github_Uploaded_code/Arrpush.js
@@ -0,0 +1,19 @@
+//  push -> use to add element in array
+let arr1 = [2,3,4,1,8,9];
+arr1.push(15);
+console.log(arr1);
+
+
+// pop -> use to remove element in  array
+
+let arraypop = [2,3,4,1,8,9]
+arraypop.pop();
+console.log(arraypop);
+
+
+
+// toString -> use to convert array to string 
+let arrayconvert = "hello mahendra hoew are you";
+// let arrtoString = []
+arrayconvert.toString();
+console.log(arrayconvert);
